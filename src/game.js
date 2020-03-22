1f08e90818dcfdcf78f0534abcdf0b49865861e1
Alexander Bonchev <alexander.b@electroneek.com>
2020-03-22 18:44:15 +0300

    added method 'isPieceOutOfBounds'

--- src/game.js
+++ src/game.js
@@ -2,7 +2,7 @@ export default class Game {
     score = 0;
     lines = 0;
     level = 0;
-    playfied = [
+    playfield = [
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
         [0,0,0,0,0,0,0,0,0,0],
@@ -31,16 +31,32 @@ export default class Game {
 
     movePieceLeft() {
         this.activePiece.x -= 1;
+
+        if(this.isPieceOutOfBounds()) {
+            this.activePiece.x += 1;
+        }
     }
 
     movePieceRight() {
         this.activePiece.x += 1;
+
+        if(this.isPieceOutOfBounds()) {
+            this.activePiece.x -= 1;
+        }
     }
 
     movePieceDown() {
         this.activePiece.y += 1;
+
+        if(this.isPieceOutOfBounds()) {
+            this.activePiece.y -= 1;
+        }
     }
     isPieceOutOfBounds() {
+        const playfield = this.playfield;
+        const {x,y} = this.activePiece;
+
+        return playfield[y] === undefined || playfield[y][x] === undefined;
 
     }
 }
